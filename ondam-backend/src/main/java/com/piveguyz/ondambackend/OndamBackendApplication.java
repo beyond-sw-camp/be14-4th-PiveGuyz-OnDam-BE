b00e047f89d26f56b8ff2213e0f2aea4995cecff
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2025-04-22 10:55:12 +0900

    [Feat] 프로젝트 생성

---
+++ ondam-backend/src/main/java/com/piveguyz/ondambackend/OndamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.piveguyz.ondambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OndamBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OndamBackendApplication.class, args);
+    }
+
+}
