456e570fbf2f146bbb5c9267fb3fff1e0ad4907e
Morris Lee <51141032+Morris235@users.noreply.github.com>
2025-04-26 14:27:18 +0900

    Revert "[feat] ChatGPT API 연동 및 응답 생성"

--- ondam-backend/src/main/java/com/piveguyz/ondambackend/analysis/command/application/service/AnalysisService.java
+++ ondam-backend/src/main/java/com/piveguyz/ondambackend/analysis/command/application/service/AnalysisService.java
@@ -1,8 +1,4 @@
 package com.piveguyz.ondambackend.analysis.command.application.service;
-import com.piveguyz.ondambackend.analysis.command.application.dto.ChatCompletionDTO;
-import java.util.Map;
 
-public interface AnalysisService {
-
-    Map<String, Object> askGpt(ChatCompletionDTO chatCompletionDto);
+public class AnalysisService {
 }
