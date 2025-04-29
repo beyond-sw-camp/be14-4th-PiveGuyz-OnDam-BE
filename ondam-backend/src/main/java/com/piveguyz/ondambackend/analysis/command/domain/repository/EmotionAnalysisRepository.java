604a001403fc905a45c129bba9254024b60eff87
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2025-04-30 00:20:54 +0900

    test: 분석 데이터 저장 테스트 구현

--- ondam-backend/src/main/java/com/piveguyz/ondambackend/analysis/command/domain/repository/EmotionAnalysisRepository.java
+++ ondam-backend/src/main/java/com/piveguyz/ondambackend/analysis/command/domain/repository/EmotionAnalysisRepository.java
@@ -3,8 +3,12 @@ package com.piveguyz.ondambackend.analysis.command.domain.repository;
 import com.piveguyz.ondambackend.analysis.command.domain.aggregate.EmotionAnalysis;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmotionAnalysisRepository extends JpaRepository<EmotionAnalysis, Long> {
     void deleteByAnalysisId(Long analysisId);
 
     long countByEmotionId(Long id);
+
+    List<EmotionAnalysis> findByAnalysisId(Long analysisId);
 }
