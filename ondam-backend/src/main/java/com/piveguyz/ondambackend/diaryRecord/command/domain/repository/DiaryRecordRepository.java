b30b1610f3922d690fa0adcaa54c79f1e0a35fa3
MJay1123 <tjalswhd1@naver.com>
2025-04-29 16:23:15 +0900

    feat: 00시마다 기존의 일기 발송 기록 만료 기능 구현 / refactor: DiaryMapper, ReplyMapper, DiaryRecordMapper mybatis 수정(is_blinded, deleted된 데이터 조회 제외)

--- ondam-backend/src/main/java/com/piveguyz/ondambackend/diaryRecord/command/domain/repository/DiaryRecordRepository.java
+++ ondam-backend/src/main/java/com/piveguyz/ondambackend/diaryRecord/command/domain/repository/DiaryRecordRepository.java
@@ -1,9 +1,16 @@
 package com.piveguyz.ondambackend.diaryRecord.command.domain.repository;
 
 import com.piveguyz.ondambackend.diaryRecord.command.domain.aggregate.DiaryRecord;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DiaryRecordRepository extends JpaRepository<DiaryRecord, Long> {
+    @Modifying
+    @Transactional
+    @Query("UPDATE DiaryRecord dr SET dr.isExpired = 'Y' WHERE dr.isExpired = 'N'")
+    int expireAllDiaryRecords();
 }
