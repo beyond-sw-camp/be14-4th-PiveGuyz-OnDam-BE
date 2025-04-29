4a904021fd3be4e120a99f2c6ec473dcc67c1569
wooseok718 <dntjr123589@naver.com>
2025-04-29 12:55:02 +0900

    feat: 회원 탈퇴 기능 구현

--- ondam-backend/src/main/java/com/piveguyz/ondambackend/member/command/application/service/MemberService.java
+++ ondam-backend/src/main/java/com/piveguyz/ondambackend/member/command/application/service/MemberService.java
@@ -1,7 +1,11 @@
 package com.piveguyz.ondambackend.member.command.application.service;
 
+import com.piveguyz.ondambackend.member.command.application.dto.ChangePasswordDTO;
 import com.piveguyz.ondambackend.member.command.application.dto.MemberDTO;
 
 public interface MemberService {
     void registMember(MemberDTO memberDTO);
+
+    void deleteMember(Long id);
+
 }
