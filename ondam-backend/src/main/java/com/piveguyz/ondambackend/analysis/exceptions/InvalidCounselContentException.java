19fe1267c8608d7c3d5aa16e140ecfcf4e6068c8
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2025-04-30 12:42:10 +0900

    chore: gpt 프롬프트 수정 및 에러 로그 수정

--- ondam-backend/src/main/java/com/piveguyz/ondambackend/analysis/exceptions/InvalidCounselContentException.java
+++ ondam-backend/src/main/java/com/piveguyz/ondambackend/analysis/exceptions/InvalidCounselContentException.java
@@ -2,6 +2,6 @@ package com.piveguyz.ondambackend.analysis.exceptions;
 
 public class InvalidCounselContentException extends IllegalArgumentException {
     public InvalidCounselContentException(String message) {
-        super("유효하지 않은 상담 내역입니다. gpt 응답 메세지 : " + message);
+        super(message);
     }
 }
