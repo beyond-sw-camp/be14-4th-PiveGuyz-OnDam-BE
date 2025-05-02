710612517996ee80f6c09d0e2e63528082a07c37
Morris235 <dltkdah777@gmail.com>
2025-05-02 14:46:34 +0900

    test : 배포 테스트

--- ondam-backend/src/main/java/com/piveguyz/ondambackend/OndamBackendApplication.java
+++ ondam-backend/src/main/java/com/piveguyz/ondambackend/OndamBackendApplication.java
@@ -5,7 +5,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
+// 테스트
 @SpringBootApplication
 @EnableScheduling
 @MapperScan(basePackages = "com.piveguyz.ondambackend", annotationClass = Mapper.class)
